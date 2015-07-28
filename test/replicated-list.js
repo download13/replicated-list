be63e986bb67960ad739171203581ba66c24d818
Erin Dachtler <download333@gmail.com>
2015-07-27 21:34:34 -0700

    Test loops and events

--- test/replicated-list.js
+++ test/replicated-list.js
@@ -23,41 +23,55 @@ describe('ReplicatedList', function() {
 	});
 
 	it('has length', function() {
-		var rl = new ReplicatedList([0, true, 'test']);
+		var list = new ReplicatedList([0, true, 'test']);
 
-		assert.equal(rl.length, 3);
+		assert.equal(list.length, 3);
 
-		rl.push(4);
+		list.push(4);
 
-		assert.equal(rl.length, 4);
+		assert.equal(list.length, 4);
 	});
 
 	it('gets', function() {
-		var rl = new ReplicatedList([0, true, 'test']);
+		var list = new ReplicatedList([0, true, 'test']);
 
-		assert.equal(rl.get(0), 0);
+		assert.equal(list.get(0), 0);
 
-		assert.equal(rl.get(1), true);
+		assert.equal(list.get(1), true);
 
-		assert.equal(rl.get(2), 'test');
+		assert.equal(list.get(2), 'test');
 
-		assert.equal(rl.get(3), undefined);
+		assert.equal(list.get(3), undefined);
 	});
 
 	it('pushes', function() {
-		var rl = new ReplicatedList();
+		var list = new ReplicatedList();
 
-		rl.push(0);
+		var events = [];
 
-		rl.push('test');
+		list.on('push', function(v) {
+			events.push(v);
+		});
 
-		rl.push(true);
+		list.push(0);
 
-		rl.push([5]);
+		list.push('test');
 
-		rl.push({t: 1});
+		list.push(true);
 
-		assert.deepEqual(rl._state, [
+		list.push([5]);
+
+		list.push({t: 1});
+
+		assert.deepEqual(list._state, [
+			0,
+			'test',
+			true,
+			[5],
+			{t: 1}
+		]);
+
+		assert.deepEqual(events, [
 			0,
 			'test',
 			true,
@@ -67,69 +81,147 @@ describe('ReplicatedList', function() {
 	});
 
 	it('pops', function() {
-		var rl = new ReplicatedList();
+		var list = new ReplicatedList();
 
-		rl.push(0);
+		var events = [];
 
-		rl.push('test');
+		list.on('pop', function(v) {
+			events.push(v);
+		});
 
-		assert.equal(rl.pop(), 'test');
+		list.push(0);
 
-		assert.deepEqual(rl._state, [0]);
+		list.push('test');
+
+		assert.equal(list.pop(), 'test');
+
+		assert.deepEqual(list._state, [0]);
+
+		assert.deepEqual(events, ['test']);
 	});
 
 	it('unshifts', function() {
-		var rl = new ReplicatedList();
+		var list = new ReplicatedList();
 
-		rl.unshift(0);
+		var events = [];
 
-		rl.unshift('test');
+		list.on('unshift', function(v) {
+			events.push(v);
+		});
 
-		rl.unshift(true);
+		list.unshift(0);
 
-		rl.unshift([5]);
+		list.unshift('test');
 
-		rl.unshift({t: 1});
+		list.unshift(true);
 
-		assert.deepEqual(rl._state, [
+		list.unshift([5]);
+
+		list.unshift({t: 1});
+
+		assert.deepEqual(list._state, [
 			{t: 1},
 			[5],
 			true,
 			'test',
 			0
 		]);
+
+		assert.deepEqual(events, [
+			0,
+			'test',
+			true,
+			[5],
+			{t: 1}
+		]);
 	});
 
 	it('shifts', function() {
-		var rl = new ReplicatedList(['test', 0]);
+		var list = new ReplicatedList(['test', 0]);
 
-		assert.equal(rl.shift(), 'test');
+		var events = [];
 
-		assert.equal(rl.shift(), 0);
+		list.on('shift', function(v) {
+			events.push(v);
+		});
+
+		assert.equal(list.shift(), 'test');
+
+		assert.equal(list.shift(), 0);
+
+		assert.deepEqual(events, [
+			'test',
+			0
+		]);
 	});
 
 	it('splices', function() {
-		var rl = new ReplicatedList([true, 'test', 0, 5, 8, 3]);
+		var list = new ReplicatedList([true, 'test', 0, 5, 8, 3]);
 
-		assert.deepEqual(rl.splice(1, 2), ['test', 0]);
+		var events = [];
 
-		assert.equal(rl.length, 4);
+		list.on('splice', function(start, remove, firstItem) {
+			events.push({s: start, r: remove, fi: firstItem});
+		});
 
-		assert.deepEqual(rl.splice(0, 0, 'added', false), []);
+		assert.deepEqual(list.splice(1, 2), ['test', 0]);
 
-		assert.deepEqual(rl._state, ['added', false, true, 5, 8, 3]);
+		assert.equal(list.length, 4);
+
+		assert.deepEqual(list.splice(0, 0, 'added', false), []);
+
+		assert.deepEqual(list._state, ['added', false, true, 5, 8, 3]);
+
+		assert.deepEqual(events, [
+			{s: 1, r: 2, fi: undefined},
+			{s: 0, r: 0, fi: 'added'}
+		]);
 	});
 
 	it('clears', function() {
+		var list = new ReplicatedList(['j', 'k', 2]);
+
+		var events = [];
+
+		list.on('clear', function() {
+			events.push(true);
+		});
+
+		assert.deepEqual(list._state, ['j', 'k', 2]);
+
+		list.clear();
+
+		assert.deepEqual(list._state, []);
+
+		assert.equal(list.length, 0);
+
+		assert.deepEqual(events, [true]);
+	});
+
+	it('loops', function() {
+		var r = [];
+
 		var rl = new ReplicatedList(['j', 'k', 2]);
 
-		assert.deepEqual(rl._state, ['j', 'k', 2]);
+		rl.forEach(function(value, index) {
+			r.push({v: value, i: index});
+		});
 
-		rl.clear();
+		assert.deepEqual(r, [
+			{v: 'j', i: 0},
+			{v: 'k', i: 1},
+			{v: 2, i: 2},
+		]);
 
-		assert.deepEqual(rl._state, []);
+		r = rl.map(function(value, index) {
+			return {v: value, i: index};
+		});
 
-		assert.equal(rl.length, 0);
+		assert.deepEqual(r, [
+			{v: 'j', i: 0},
+			{v: 'k', i: 1},
+			{v: 2, i: 2},
+		]);
 	});
 
 	it('takes cmds', function() {
